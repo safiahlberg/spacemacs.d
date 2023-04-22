3509a85f4ac4eb8a8c87aea78ef7d0eb400df0b7
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2023-04-22 23:01:35 +0200

    Added Window sizing and positioning

--- user-config.el
+++ user-config.el
@@ -16,3 +16,22 @@
 (evil-global-set-key 'normal "/" 'helm-swoop)
 ;;
 ;; ---------------------------------------
+
+;; Set window size
+(setq resize-factor 0.45)
+(setq width-breakpoint 1440)
+(setq pos-adjustment 0)
+(when window-system
+  (let ((frame (selected-frame))
+        (resized-height (/ (display-pixel-height) (frame-char-height)))
+        (resized-width (let ((resized-width (-  (/ (display-pixel-width) (frame-char-width)) pos-adjustment)))
+                         (if (> (display-pixel-width) width-breakpoint)
+                             (round (* resized-width resize-factor))
+                           resized-width))))
+    (set-frame-height frame resized-height)
+    (set-frame-width frame resized-width)
+    (set-frame-position frame
+                        (if (> (display-pixel-width) width-breakpoint)
+                            (+ (- (display-pixel-width) (round (* (display-pixel-width) resize-factor))) pos-adjustment)
+                          0) 0))
+  )
