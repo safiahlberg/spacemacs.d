467fec760542e4dc1fb28ab0fc297167a49c7340
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2023-04-22 23:38:16 +0200

    Changed theme, and height

--- init.el
+++ init.el
@@ -407,8 +407,8 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(doom-gruvbox
-                         doom-gruvbox-light
+   dotspacemacs-themes '(doom-gruvbox-light
+                         doom-gruvbox
                          spacemacs-dark
                          spacemacs-light)
 
