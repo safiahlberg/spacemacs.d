54ca37a6c4fd037bd9af97a73b2dc2e8d8c3a40f
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2023-07-26 20:08:12 +0200

    Added org publish

--- user-config.el
+++ user-config.el
@@ -35,3 +35,32 @@
                             (+ (- (display-pixel-width) (round (* (display-pixel-width) resize-factor))) pos-adjustment)
                           0) 0))
   )
+
+;; ---------------------------------------
+;;
+;; Org publish
+;;
+;; https://orgmode.org/worg/org-tutorials/org-publish-html-tutorial.html
+;; ---------------------------------------
+(require 'ox-publish)
+(setq org-publish-project-alist
+      '(
+        ("org-notes"
+         :base-directory "~/Documents/org-roam/"
+         :base-extension "org"
+         :publishing-directory "~/public_html/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :auto-preamble t
+         )
+        ("org-static"
+         :base-directory "~/Documents/org-roam/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "~/public_html/"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )
+        ("org"
+         :components ("org-notes" "org-static"))
+        ))
